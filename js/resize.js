debc8a762ad03473fd067644dad6f1a4013d2065
Belrestro <swerswer>
2015-05-12 12:48:31 +0300

    nothing

--- js/resize.js
+++ js/resize.js
@@ -2,7 +2,9 @@ function resize() {
               var client = document.documentElement.clientWidth,
                   list = document.getElementsByClassName('menu_list'),
                   logimg = document.getElementById('img_logo'),
-                  slogan = document.getElementById('slogan');
+                  slogan = document.getElementById('slogan'),
+                  rightbutton = document.getElementsByClassName('owl-prev'),
+                  leftbutton = document.getElementsByClassName('owl-next');
 
                 if (client <= 850) {
                   for(i=0;i<list.length;i++) {
@@ -12,6 +14,8 @@ function resize() {
                   logimg.style.display = "none";
                   slogan.style.fontSize = "34px";
                   slogan.style.lineHeight = "36px";
+                  leftbutton[0].style.right = "12%";
+                  rightbutton[0].style.left = "12%";
                 } 
                 /*if (client <= 550) {
                   for(i=0;i<list.length;i++) {
@@ -29,6 +33,8 @@ function resize() {
                   list[2].style.marginLeft = "29%";
                   slogan.style.fontSize = "48px";
                   slogan.style.lineHeight = "36px";
+                  leftbutton[0].style.right = "0";
+                  rightbutton[0].style.left = "0";
                 }
             }
             resize();
